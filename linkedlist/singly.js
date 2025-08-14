96e988d4d518eed8b74e5593c64c89a778883ff9
Raisa Toscano <daisa1690@gmail.com>
2025-08-13 23:13:32 -0600

    LinkedList data structure

---
+++ linkedlist/singly.js
@@ -0,0 +1,39 @@
+//visual example
+
+// 1--> 2--> 3--> 4--> 5--> null;
+
+//let singlyLinkedList = {
+// head: {
+//    value: 1,
+//    next: {
+//        value: 2,
+//        next: {
+//            value: 3,
+//            next: {
+//                value: 4,
+//                next: null,
+//            },
+//        },
+//    },
+//  },
+//}
+class MySinglyLinkedList {
+    constructor(value) {
+        this.head = {
+            value: value,
+            next: null,
+        }
+        this.tail = this.head;
+
+        this.length = 1;
+    }
+}
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+let myLinkedList = new MySinglyLinkedList(1);
+
